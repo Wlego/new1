519b7e9ad3a190ac5b7fc746be3de0d2a0ea534f
unknown <ENG@HIS0141.KGPTO.local>
2021-01-14 02:58:15 +0300

    style reg

--- reg.php
+++ reg.php
@@ -68,80 +68,87 @@
 		<div class="clear"><br /></div>
 	
 	<div class="container">
-	<h2>Регистрация</h2>
-	
-<form class="form-horizontal">
-<div>
-	<div class="avatar col-xs-3">
-		<a class="thumbnail "><span class="glyphicon glyphicon-user" style="font-size:130px;"></span></a>
-	</div>
-	<div class="col-xs-9">
-	<div class="form-group">
-		<label class="control-label col-xs-3" for="lastName">Фамилия:</label>
-		<div class="col-xs-9">
-		<input type="text" class="form-control" id="lastName" placeholder="Введите фамилию">
+		<div class="col-sm-9">
+		
+		
+			<form class="form-horizontal">
+			<div class="container">
+				<h2>Регистрация</h2>
+				<div class="clear"><br /></div>
+				<div class="col-xs-3">
+				<div class="avatar">
+					<a class="thumbnail "><span class="glyphicon glyphicon-user" style="font-size:100"></span></a>
+				</div>
+				</div>
+				<div class="col-xs-9">
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="lastName">Фамилия:</label>
+					<div class="col-xs-9">
+					<input type="text" class="form-control" id="lastName" placeholder="Введите фамилию">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="firstName">Имя:</label>
+					<div class="col-xs-9">
+					<input type="text" class="form-control" id="firstName" placeholder="Введите имя">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="fatherName">Отчество:</label>
+					<div class="col-xs-9">
+					<input type="text" class="form-control" id="fatherName" placeholder="Введите отчество">
+					</div>
+				</div>
+				</div>
+					
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="inputEmail">Email:</label>
+					<div class="col-xs-9">
+					<input type="email" class="form-control" id="inputEmail" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="inputPassword">Пароль:</label>
+					<div class="col-xs-9">
+					<input type="password" class="form-control" id="inputPassword" placeholder="Введите пароль" autocomplete="off">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="confirmPassword">Подтвердите пароль:</label>
+					<div class="col-xs-9">
+					<input type="password" class="form-control" id="confirmPassword" placeholder="Введите пароль ещё раз" autocomplete="off">
+					</div>
+				</div> 
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="phoneNumber">Телефон:</label>
+					<div class="col-xs-9">
+					<input type="tel" class="form-control" id="phoneNumber" placeholder="Введите номер телефона">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-xs-3" for="postalAddress">Адрес:</label>
+					<div class="col-xs-9">
+					<textarea rows="3" class="form-control" id="postalAddress" placeholder="Введите адрес"></textarea>
+					</div>
+				</div>
+				<div class="form-group">
+				<div class="col-xs-offset-3 col-xs-9">
+				<label class="checkbox-inline">
+					<input type="checkbox" value="agree">  Я согласен с <a href="#">условиями</a>
+				</label>
+				</div>
+				</div>	
+			</div>
+			</form>
+			<br />
+			<div class="form-group">
+				<div class="col-xs-offset-3 col-xs-9">
+				<button onclick="post_query('/new1/gform.php', 'reg', 'lastName.firstName.fatherName.inputEmail.inputPassword.phoneNumber.postalAddress')"  class="btn btn-default reg" value="Регистрация">Зарегистрироваться</button>
+				<!-- <button class="reg">0</button> -->
+				</div>
+			</div>	
 		</div>
 	</div>
-	<div class="form-group">
-		<label class="control-label col-xs-3" for="firstName">Имя:</label>
-		<div class="col-xs-9">
-		<input type="text" class="form-control" id="firstName" placeholder="Введите имя">
-		</div>
-	</div>
-	<div class="form-group">
-		<label class="control-label col-xs-3" for="fatherName">Отчество:</label>
-		<div class="col-xs-9">
-		<input type="text" class="form-control" id="fatherName" placeholder="Введите отчество">
-		</div>
-	</div>
-	</div>
-</div>	
-  <div class="form-group">
-    <label class="control-label col-xs-3" for="inputEmail">Email:</label>
-    <div class="col-xs-9">
-      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-    </div>
-  </div>
-   <div class="form-group">
-    <label class="control-label col-xs-3" for="inputPassword">Пароль:</label>
-    <div class="col-xs-9">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Введите пароль" autocomplete="off">
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="control-label col-xs-3" for="confirmPassword">Подтвердите пароль:</label>
-    <div class="col-xs-9">
-      <input type="password" class="form-control" id="confirmPassword" placeholder="Введите пароль ещё раз" autocomplete="off">
-    </div>
-  </div> 
-  <div class="form-group">
-    <label class="control-label col-xs-3" for="phoneNumber">Телефон:</label>
-    <div class="col-xs-9">
-      <input type="tel" class="form-control" id="phoneNumber" placeholder="Введите номер телефона">
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="control-label col-xs-3" for="postalAddress">Адрес:</label>
-    <div class="col-xs-9">
-      <textarea rows="3" class="form-control" id="postalAddress" placeholder="Введите адрес"></textarea>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-xs-offset-3 col-xs-9">
-      <label class="checkbox-inline">
-        <input type="checkbox" value="agree">  Я согласен с <a href="#">условиями</a>
-      </label>
-    </div>
-  </div>
-</form>
-<br />
-<div class="form-group">
-    <div class="col-xs-offset-3 col-xs-9">
-	<button onclick="post_query('/new1/gform.php', 'reg', 'lastName.firstName.fatherName.inputEmail.inputPassword.phoneNumber.postalAddress')"  class="btn btn-default reg" value="Регистрация">Зарегистрироваться</button>
-	<button class="reg">0</button>
-	</div>
-</div>	
-</div>
  
 	<div id="myModalBoxEnter" class="modal fade" data-backdrop="false">
 		<div class="modal-dialog">
